d2483c460a7aaf7a074571203c40caf37dc9ff8c
Murat OZ <murat.ozeee@gmail.com>
2023-02-17 08:28:24 +0300

    .

---
+++ ValueReferanceType/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ValueReferanceType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 10;
+            int num2 = 20;
+
+            num1= num2;
+            num2 = 70;
+
+            //what is the num1 ? 
+
+            //num1=20 is result
+            Console.WriteLine(num1);
+
+            int[] number1 = new int[] { 150, 200, 300, 400 };
+            int[] number2 = new int[] { 500, 600, 700, 800, 900 };
+
+            number1 = number2;
+
+            number2[0] = 999;
+            Console.WriteLine(number1[0]);
+
+            //what is the number1[0] ??
+            
+            //number1[0]=12 is Result
+
+
+
+
+        }
+    }
+}
