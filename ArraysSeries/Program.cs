2807f8239d1ee39143815df7954ebc90774f093e
Murat OZ <murat.ozeee@gmail.com>
2023-02-17 19:49:18 +0300

    .

---
+++ ArraysSeries/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ArraysSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
