ad254f3d02550a6afb610cb9b6e4503e5bcb48ad
Murat OZ <murat.ozeee@gmail.com>
2023-02-22 01:34:26 +0300

    inheritance with Animal Methods

---
+++ İntheritance_Meaning_Example/AnimalManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+{
+    public class AnimalManager
+    {
+        public string animalName;
+        public string animalGender;
+        public string animalColor;
+        public int animalAge;
+        public void AnimalInformation()
+        {
+            Console.WriteLine("Animal Name ="+animalName);
+            Console.WriteLine("Animal Gender ="+animalGender);
+            Console.WriteLine("Animal Color ="+animalColor);
+            Console.WriteLine("Animal Age ="+animalAge);
+
+        }
+    }
+}
---
+++ İntheritance_Meaning_Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using InheritanceExample;
+
+namespace İntheritance_Meaning_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Cat cat= new Cat();
+
+            cat.animalName = "Zeus";
+            cat.animalGender = "Male";
+            cat.animalAge = 3;
+            cat.animalColor = "Black";
+
+            cat.AnimalInformation();
+
+
+
+
+
+        }
+    }
+}
